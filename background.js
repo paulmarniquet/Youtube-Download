23b47e8d9aa338e75d34b4d77c89a6a7b5289d4c
paul.marniquet <paul.marniquet@epitech.eu>
2022-10-15 18:12:15 +0200

    [+] Added Basics elements [+]

--- background.js
+++ background.js
@@ -1,12 +1,16 @@
+chrome.tabs.onUpdated.addListener(valid_url);
+chrome.pageAction.onClicked.addListener(GetURL);
 
+function valid_url(tabId, changeInfo, tab) {
+	if(typeof tab != "undefined" && typeof tab != "null" ) {
+		if (/youtube[.]com\/watch\?v=/.test(tab.url)) {
+			chrome.pageAction.show(tabId);
+		}
+	}
+};
 
-
-
-	chrome.browserAction.onClicked.addListener(function(tab) {
-	  
-	  chrome.tabs.executeScript({
-		code: ''
-	  });
-	  
-	  
-	});
+function GetURL() {
+	$.getJSON('https://noembed.com/embed',
+		{format: 'json', url: document.URL}, function (data)
+		{alert(data.title);});
+}
