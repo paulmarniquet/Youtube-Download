64c6e17c3aae4d03d8cbe13d8f05bacb8a50c5e0
paul.marniquet <paul.marniquet@epitech.eu>
2022-10-21 11:45:44 +0200

    [+] Found Youtube Downloader API and implmented it [+]

--- background.js
+++ background.js
@@ -1,9 +1,17 @@
 chrome.tabs.onUpdated.addListener(valid_url);
 
 function valid_url(tabId, changeInfo, tab) {
-	//if(typeof tab != "undefined" && typeof tab != "null" ) {
-		//if (/youtube[.]com\/watch\?v=/.test(tab.url)) {
+	if(typeof tab != "undefined" && typeof tab != "null" ) {
+		if (/youtube[.]com\/watch\?v=/.test(tab.url)) {
 			chrome.pageAction.show(tabId);
-		//}
-	//}
+		}
+	}
 };
+
+window.onload = function() {
+	chrome.tabs.getSelected(null, function(tab) {
+		var url = tab.url;
+		var apiUrl = "https://convert2mp3s.com/api/widgetv2?url=" + url;
+		document.getElementById('iframe').src = apiUrl;
+	});
+}
